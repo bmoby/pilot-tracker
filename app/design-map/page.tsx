5c6dac1d2fdc45ba3b68f96e4dedff7c1a0d77ef
Tsarag <msouleymanov@gmail.com>
2026-05-14 21:21:25 +0700

    Добавить дизайн-карту интерфейса

---
+++ app/design-map/page.tsx
@@ -0,0 +1,10 @@
+import { DesignMapPage } from "@/ui/design-map-page";
+
+export const metadata = {
+  title: "Дизайн-карта | Pilot Tracker",
+  description: "Внутренняя дизайн-карта интерфейса Pilot Tracker.",
+};
+
+export default function DesignMapRoute() {
+  return <DesignMapPage />;
+}
